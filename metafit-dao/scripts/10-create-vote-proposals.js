410f50f6df0baf8ba46006d2a6c457db36dcb754
envs <olaonipekun2001@yahoo.com>
2022-06-27 10:32:33 +0200

    Addition of Voting Proposal file

--- metafit-dao/scripts/10-create-vote-proposals.js
+++ metafit-dao/scripts/10-create-vote-proposals.js
@@ -1,3 +1,61 @@
 import sdk from "./1-initialize-sdk.js";
 import { ethers } from "ethers";
 
+// Governance contract
+const vote = sdk.getVote("0x42c715c6c136A91F3CA060C83DC1D9421fFcE966");
+// ERC-20 contract
+const token = sdk.getToken("0x4995818d1DDDaE65E763985955a3AC950492f242");
+
+(async () => {
+    try {
+        // Create proposal to mint 500,000 new token to treasury
+        const amount = 500_000;
+        const description = "Should the DAO mint an additional " + amount + " tokens into the treasury?";
+        const executions = [
+            {
+                // Token contract that actually executes the mint
+                toAddress: token.getAddress(),
+                // nativeToken is ETH. Hence, nativeTokenValue is the amount of ETH we want to send in this proposal.
+                nativeTokenValue: 0,
+                transactionData: token.encoder.encode(
+                    "mintTo", [
+                    vote.getAddress(),
+                    ethers.utils.parseUnits(amount.toString(), 18),
+                ]
+                ),
+            }
+        ];
+
+        await vote.propose(description, executions);
+
+        console.log("✅ Successfully created proposal to mint tokens");
+
+    } catch (error) {
+        console.error("Failed to create first proposal", error);
+        process.exit(1);
+    }
+
+    try {
+        // Create proposal to transfer ourselves 5,000 tokens for being awesome.
+        const amount = 5_000;
+        const description = "Should the DAO transfer " + amount + " tokens from the treasury to " +
+            process.env.WALLET_ADDRESS + " for being awesome?";
+        const executions = [
+            {
+                // Token contract that actually executes the mint
+                toAddress: token.getAddress(),
+                // nativeToken is ETH. Hence, nativeTokenValue is the amount of ETH we want to send in this proposal.
+                nativeTokenValue: 0,
+                transactionData: token.encoder.encode(
+                    "mintTo", [
+                    vote.getAddress(),
+                    ethers.utils.parseUnits(amount.toString(), 18),
+                ]
+                ),
+            }
+        ];
+    } catch (error) {
+
+    }
+
+})();
